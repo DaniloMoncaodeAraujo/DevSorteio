b4545edeada4eb2a4935e452f9ed9803662d345f
DaniloMoncaodeAraujo <danilomoncaoaraujo>
2024-09-13 09:04:10 -0300

    Primeiro Commit!

---
+++ script.js
@@ -0,0 +1,12 @@
+const button = document.querySelector("button")
+
+button.addEventListener("click", sortear)
+
+function sortear() {
+    const min = Math.ceil(document.querySelector(".input-min").value)
+    const max = Math.floor(document.querySelector(".input-max").value)
+
+    const resultado = Math.floor(Math.random() * (max - min + 1)) + min;
+
+    alert(resultado)
+}
